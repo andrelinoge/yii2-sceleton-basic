7659056db7eaeb7066d60de3d2d0cc2b47ffa6e4
Andre Linoge <andrelinoge87@gmail.com>
2015-03-12 10:28:25 +0200

    added user preview for dashboard

--- modules/dashboard/views/users/view.php
+++ modules/dashboard/views/users/view.php
@@ -1,18 +1,16 @@
 <?php
 
 use yii\helpers\Html;
-use yii\grid\GridView;
 use yii\widgets\DetailView;
-use yii\widgets\Pjax;
 
 /**
 * @var yii\web\View $this
-* @var app\models\Post $model
+* @var app\models\User $model
 */
 
-$this->title = 'Post View ' . $model->title . '';
+$this->title = 'User ' . $model->name . '';
 $this->params['breadcrumbs'][] = ['label' => 'Posts', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => (string)$model->title, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => (string)$model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'View';
 ?>
 <div class="post-view">
@@ -20,8 +18,6 @@ $this->params['breadcrumbs'][] = 'View';
     <p class='pull-left'>
         <?= Html::a('<span class="glyphicon glyphicon-pencil"></span> Edit', ['update', 'id' => $model->id],
         ['class' => 'btn btn-info']) ?>
-        <?= Html::a('<span class="glyphicon glyphicon-plus"></span> New Post', ['create'], ['class' => 'btn
-        btn-success']) ?>
     </p>
 
         <p class='pull-right'>
@@ -29,52 +25,39 @@ $this->params['breadcrumbs'][] = 'View';
     </p><div class='clearfix'></div> 
 
     
-    <h3>
-        <?= $model->title ?>    </h3>
+    <h3><?= $model->name ?></h3>
 
 
-    <? $this->beginBlock('\app\models\Post'); ?>
+    <? $this->beginBlock('\app\models\User'); ?>
 
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            [   
-                'attribute' => 'image',
-                'format'    => 'html',
-                'value'     => Html::img($model->getSrc('s'))
-            ],
     		'id',
-    		'title',
-            'slug',
-    		'content:ntext',
-    		'category_id',
-    		'created_at',
+    		'name',
+            'email',
+    		'created_at:date',
         ],
     ]); ?>
 
     <hr/>
 
-    <?php echo Html::a('<span class="glyphicon glyphicon-trash"></span> Delete', ['delete', 'id' => $model->id],
+    <?= Html::a('<span class="glyphicon glyphicon-trash"></span> Delete', ['delete', 'id' => $model->id],
     [
     'class' => 'btn btn-danger',
     'data-confirm' => Yii::t('app', 'Are you sure to delete this item?'),
     'data-method' => 'post',
     ]); ?>
 
-    <?php $this->endBlock(); ?>
+    <? $this->endBlock(); ?>
 
-
-    
-    <?=
-    \yii\bootstrap\Tabs::widget(
-                 [
-                     'id' => 'relation-tabs',
-                     'encodeLabels' => false,
-                     'items' => [ [
-    'label'   => '<span class="glyphicon glyphicon-asterisk"></span> Post',
-    'content' => $this->blocks['\app\models\Post'],
-    'active'  => true,
-], ]
-                 ]
-    );
-    ?></div>
+    <?= \yii\bootstrap\Tabs::widget([
+        'id' => 'relation-tabs',
+        'encodeLabels' => false,
+        'items' => [ [
+            'label'   => '<span class="glyphicon glyphicon-asterisk"></span> User',
+            'content' => $this->blocks['\app\models\User'],
+            'active'  => true,
+        ], ]
+    ]) ?>
+</div>
